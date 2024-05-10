a8f03327af027cca7774a763fa5a97a92541e7fe
AlexMaltsev86 <malets86@mail.ru>
2024-05-10 11:51:51 +0300

    Добавлены файлы практическое задание 1

--- lab1/pipline.sh
+++ lab1/pipline.sh
@@ -1,1 +1,5 @@
-
+#!/bin/sh
+python3 ./mlopsPractice/lab1/data_creation.py
+python3 ./mlopsPractice/lab1/model_preprocessing.py
+python3 ./mlopsPractice/lab1/model_preparation.py
+python3 ./mlopsPractice/lab1/model_testing.py
